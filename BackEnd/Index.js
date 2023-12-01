ae522f54bc714ad3e4bd822c577ab493347087fe
Carlo <carloggiacometti77@gmail.com>
2023-12-01 01:11:42 -0300

    headers responsivos, postar exercício (v professor), outros

--- BackEnd/Index.js
+++ BackEnd/Index.js
@@ -1,10 +1,22 @@
+// POP UP DE LOGIN
+
 function abrirPopUp() {
     // Defina o tamanho do pop-up como um quadrado de 328x328 pixels, mantendo a mesma proporção
     var size = Math.min(window.innerWidth * 0.8, window.innerHeight * 0.8, 508);
     var width = size;
-    var height = size; 
+    var height = size;
     var left = (window.innerWidth - width) / 2; // Centralize o pop-up horizontalmente
     var top = (window.innerHeight - height) / 2; // Centralize o pop-up verticalmente
-    
+
     var popup = window.open('login.html', 'popup', 'width=' + width + ',height=' + height + ',left=' + left + ',top=' + top);
+}
+
+// POP UP PARA POSTAR EXERCÍCIOS
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
 }
