51234be0c08fcee43e05792118ca6d0d7c3d977d
Carlo <carloggiacometti77@gmail.com>
2023-12-03 15:36:31 -0300

    nova página para javascript do login, BackEnd/login.js

---
+++ BackEnd/Login.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Pega o Form pelo ID
+    const form = document.querySelector("form");
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault(); // Impede o envio padrão do formulário
+
+        // Pega os valores do email e password do login.html
+        const email = document.getElementById("login").value;
+        const password = document.getElementById("passwordField").value;
+
+        // Cria um objeto com os dados de email e senha
+        const data = {
+            username: email,
+            password: password,
+        };
+
+        // Envia solicitação POST para o servidor Django
+        fetch("http://localhost:8080/token/", {
+            method: "POST", // Método para solicitar
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data), // Converte o objeto de dados em uma string .json
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                // Ver no console a resposta do servidor
+                console.log("Token de acesso:", data.access);
+                console.log("Token de atualização:", data.refresh);
+            })
+            .catch((error) => {
+                console.error("Erro ao enviar requisição:", error);
+            });
+    });
+});
