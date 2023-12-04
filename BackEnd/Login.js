4d2ec7d0c35009123a5a7e7cb892b329638ff741
Carlo <carloggiacometti77@gmail.com>
2023-12-03 23:41:46 -0300

    login.js nao tinha ido

--- BackEnd/Login.js
+++ BackEnd/Login.js
@@ -16,60 +16,21 @@ document.addEventListener("DOMContentLoaded", function () {
         };
 
         // Envia solicitação POST para o servidor Django
-        fetch("http://localhost:8080/token", {
+        fetch("http://localhost:8080/token/", {
             method: "POST", // Método para solicitar
             headers: {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify(data), // Converte o objeto de dados em uma string .json
         })
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-
-            const contentType = response.headers.get('content-type');
-            if (contentType && contentType.includes('application/json')) {
-                return response.json();
-            } else {
-                throw new TypeError('A resposta do servidor não é JSON válido');
-            }
-        })
-        .then((data) => {
-            // Busca informações do usuário
-            fetch("http://localhost:8080/aluno_info/", {
-                headers: {
-                    Authorization: `Bearer ${data.access}`,
-                },
-            })
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                }
-
-                const contentType = response.headers.get('content-type');
-                if (contentType && contentType.includes('application/json')) {
-                    return response.json();
-                } else {
-                    throw new TypeError('A resposta do servidor não é JSON válido');
-                }
-            })
-            .then((userData) => {
-                // Atualiza o nome do aluno na página principal.html
-                document.getElementById("nomeAluno").innerText = `${userData.first_name} ${userData.last_name}`;
-                // Redireciona para a página principal.html
-                window.location.href = "principal.html";
+            .then((response) => response.json())
+            .then((data) => {
+                // Ver no console a resposta do servidor
+                console.log("Sucesso! Usuário logado, Token de acesso:", data.access);
+                console.log("Token de atualização:", data.refresh);
             })
             .catch((error) => {
-                console.error("Erro ao obter informações do usuário:", error);
+                console.error("Erro ao enviar requisição:", error);
             });
-
-            // Ver no console a resposta do servidor
-            console.log("Sucesso! Usuário logado, Token de acesso:", data.access);
-            console.log("Token de atualização:", data.refresh);
-        })
-        .catch((error) => {
-            console.error("Erro ao enviar requisição:", error);
-        });
     });
 });
